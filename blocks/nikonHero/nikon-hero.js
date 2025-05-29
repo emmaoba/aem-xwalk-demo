1966fcb1f971ea76c566093d96eb254da352fd64
Emmanuel Obaseki <emmanuelobaseki@gmail.com>
2025-05-29 21:06:57 +0900

    Update nikon-hero.js

--- blocks/nikonHero/nikon-hero.js
+++ blocks/nikonHero/nikon-hero.js
@@ -1,6 +1,32 @@
 export default function decorate(block) {
-    const [titleWrapper, textWrapper, imageWrapper] = block.children;
-    console.log('titleWrapper', titleWrapper);
-    console.log('textWrapper', textWrapper);
-    console.log('imageWrapper', imageWrapper);
+    const [headingLevelWrapper] = document.querySelector('.nikon-hero-wrapper').children;
+    let headingLevel;
+
+    if(headingLevelWrapper.className.includes('h1')) {
+        headingLevel = 'h1';
+    }
+    else if(headingLevelWrapper.className.includes('h2')) {
+        headingLevel = 'h2';
+    }
+    else if(headingLevelWrapper.className.includes('h3')) {
+        headingLevel = 'h3';
+    }
+    else if(headingLevelWrapper.className.includes('h4')) {
+        headingLevel = 'h4';
+    }
+    appendHeading(block, headingLevel);
+}
+
+function appendHeading(block, headingLevel) {
+    const [titleWrapper] = block.children;
+    // Hタグを作成
+    const heading = document.createElement(headingLevel);
+    // 整形前のPタグを取得
+    const pTag = titleWrapper.querySelector('div > div > p');
+    // Hタグにクラスを追加
+    heading.className = 'nikon-hero-heading';
+    // PタグのテキストをHタグに設定
+    heading.textContent = pTag.textContent.trim();
+    //既存のPタグをHタグに置き換える
+    pTag.replaceWith(heading);
 }
